9b1ee236d5dba4d9bafc858f95937962e431a315
kavya <kavya.p@algonox.com>
2019-10-01 13:03:42 +0530

    test

--- planner_ongoing_dashboard.php
+++ planner_ongoing_dashboard.php
@@ -80,6 +80,8 @@ $(document).ready(function () {
 </script>
 </head>
 
+
+
 <script type="text/javascript">
 
     // #1: Set up ADAL
@@ -123,7 +125,74 @@ $(document).ready(function () {
                             console.log(xhr.responseText);
                             var response_fromAD = xhr.responseText
                             console.log(response_fromAD);
-
+                           //  currentdate = new Date().toLocaleString();
+                           //  sendObj = {};
+                           //  sendObj.responsefromad = response_fromAD;
+                           //  sendObj.currentdate = currentdate;
+                           //  console.log(sendObj);
+                           //  var form = new FormData();
+                           //  form.append("file", JSON.stringify(sendObj));
+                           //  var settings11 = {
+                           //      "async": true,
+                           //      "crossDomain": true,
+                           //      "url": aws_url+'Login_ad',
+                           //      "method": "POST",
+                           //      "processData": false,
+                           //      "contentType": false,
+                           //      "mimeType": "multipart/form-data",
+                           //      "data": form
+                           //  };
+                           //  $.ajax(settings11).done(function (msg) {
+                           //     msg = JSON.parse(msg);
+                           //     console.log(msg);
+                           //     localStorage.setItem("allprevialges",JSON.stringify(msg.privilegers))
+                           //     sessionStorage.setItem("isnewuser",msg.isnewuser)
+                           //     sessionStorage.setItem("role",msg.role)
+                           //     sessionStorage.setItem("useremail",msg.useremail)
+                           //     sessionStorage.setItem("userid",msg.user_id)
+                           //     sessionStorage.setItem("sessionidd",msg.sessionid)
+                           //     sessionStorage.setItem("usernamee",msg.username)
+                           //     sessionStorage.setItem("isprofile", msg.isprofile_created)
+                           //     if (msg.validlogin == "true") {
+                           //         login_obj = msg;
+                           //
+                           //         console.log(login_obj);
+                           //
+                           //         console.log(msg);
+                           //
+                           //         if(msg.role == "Admin"){
+                           //             window.location.href="adminindex.php";
+                           //         }
+                           //         else {
+                           //             if (msg.isnewuser == "true") {
+                           //                 window.location.href="userprofile.php";
+                           //             }
+                           //             else {
+                           //                 if (msg.isprofile_created == "true") {
+                           //                     window.location.href="planner_ongoing_dashboard.php";
+                           //                 }
+                           //                 else {
+                           //                     window.location.href="userprofile.php";
+                           //                 }
+                           //             }
+                           //         }
+                           //     }
+                           //     else {
+                           //         swal("Invalid username/password")
+                           //     }
+                           //
+                           //     $.ajax({
+                           //         url: 'assets/backgrounds/session_create.php',
+                           //         method: 'POST',
+                           //         data: login_obj,
+                           //
+                           //         success: function (msg) {
+                           //             console.log(msg);
+                           //
+                           //         }
+                           //     })
+                           //
+                           // })
 
                         } else {
                             // TODO: Do something with the error
