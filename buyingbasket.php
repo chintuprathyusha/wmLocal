310c0d470aed26357db6865d273743c5ef61b1bf
kavya <kavya.p@algonox.com>
2019-10-01 18:00:24 +0530

    testing

--- buyingbasket.php
+++ buyingbasket.php
@@ -86,12 +86,12 @@ if ($_SESSION['usernamee'] == '') {
 	<script src="global_assets/js/demo_pages/form_input_groups.js"></script>
 	<script src="assets/js/plannerongoing.js"></script>
 	<script src="assets/js/sidenavjscode.js"></script>
-	<script src="assets/js/buyingbasket.js"></script>
 	<script src="assets/js/sweetalert.min.js"></script>
-	<script src="assets/js/common.js" charset="utf-8"></script>
+	<!-- <script src="assets/js/common.js" charset="utf-8"></script> -->
 	<link href="assets/css/jquery-confirm.css" rel="stylesheet" type="text/css">
 <script src="assets/js/jquery-confirm.js" charset="utf-8"></script>
 <?php include 'assets/includes/common_scripts.php';?>
+<script src="assets/js/buyingbasket.js"></script>
 <!-- <script src="assets/js/common.js" charset="utf-8"></script> -->
 	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script> -->
