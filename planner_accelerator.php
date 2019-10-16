ee9f4ea27a3e33d1633be346c36ebbfe2cfc6962
Shehnaz <shehnaz.shaik@algonox.com>
2019-10-16 19:40:14 +0530

    accelerator

--- planner_accelerator.php
+++ planner_accelerator.php
@@ -6,55 +6,13 @@ if ($_SESSION['usernamee'] == '') {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- Mirrored from demo.interface.club/limitless/demo/bs4/Template/layout_1/LTR/default/full/uploader_bootstrap.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 12 Jun 2019 06:48:40 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Wavemaker - WM FLOW</title>
-
-	<!-- Global stylesheets -->
-	<!-- <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css"> -->
-	<link href="assets/css/fonts.css" rel="stylesheet" type="text/css">
-	<link href="global_assets/css/icons/icomoon/styles.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/layout.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/common.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/components.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/colors.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/sweetalert.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/jquery-confirm.css" rel="stylesheet" type="text/css">
-	<script src="assets/js/jquery-confirm.js" charset="utf-8"></script>
-	<!-- /global stylesheets -->
-
-	<!-- Core JS files -->
-	<script src="global_assets/js/main/jquery.min.js"></script>
-	<script src="global_assets/js/main/bootstrap.bundle.min.js"></script>
-	<script src="global_assets/js/plugins/loaders/blockui.min.js"></script>
-	<!-- /core JS files -->
-
-	<!-- Theme JS files -->
-	<script src="global_assets/js/plugins/uploaders/fileinput/plugins/purify.min.js"></script>
-	<script src="global_assets/js/plugins/uploaders/fileinput/plugins/sortable.min.js"></script>
-	<script src="global_assets/js/plugins/uploaders/fileinput/fileinput.min.js"></script>
-	<script src="assets/js/app.js"></script>
-	<script src="global_assets/js/demo_pages/uploader_bootstrap.js"></script>
-    <script src="global_assets/js/plugins/forms/styling/uniform.min.js"></script>
-	<script src="global_assets/js/plugins/forms/styling/switchery.min.js"></script>
-	<script src="global_assets/js/plugins/forms/styling/switch.min.js"></script>
-    <script src="global_assets/js/demo_pages/form_checkboxes_radios.js"></script>
-	<script src="global_assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
-	<script src="global_assets/js/plugins/forms/selects/select2.min.js"></script>
-	<script src="global_assets/js/demo_pages/form_select2.js"></script>
-	<script src="global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
-	<script src="global_assets/js/demo_pages/datatables_sorting.js"></script>
-	<script src="assets/js/sidenavjscode.js"></script>
-	<!-- <script src="assets/js/common.js"></script> -->
-	<script src="assets/js/azurecode.js"></script>
-	<script src="assets/js/sweetalert.min.js"></script>
+	<?php include 'assets/includes/common_css.php'; ?>
 	<?php include 'assets/includes/common_scripts.php';?>
 	<script src="assets/js/accelerator.js"></script>
 	<script>
@@ -119,17 +77,8 @@ if ($_SESSION['usernamee'] == '') {
 
 
 	})
-	  // var username = sessionStorage.getItem("usernamee");
-	  //debugger
-		// if (username ==  null) {
-		//     window.location.href="index.php";
-		// }
 
 	</script>
-
-	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script> -->
-	<!-- /theme JS files -->
 
 </head>
 <style>
@@ -178,50 +127,6 @@ if ($_SESSION['usernamee'] == '') {
 }
 </style>
 
-<!-- <script>
-$( document ).ready(function() {
-	$(".budget").hide();
-	$("body").on("click", ".cprp_r", function(){
-		$(".cprp").show();
-		$(".budget").hide();
-	})
-	$("body").on("click", ".budget_r", function(){
-		//debugger
-		$(".budget").show();
-		$(".cprp").hide();
-		$("#cprp_upload").hide();
-		$(".complete_cprp").hide();
-	})
-	$("#cprp_upload").hide();
-	$(".complete_cprp").hide();
-	$(".complete_budget").hide();
-	$("#budget_upload").hide();
-	$("body").on("click", ".cprp_submit", function(){
-		// $(this).hide();
-		$("#cprp_upload").show();
-		$(".cprp").hide();
-		$(".complete_cprp").show();
-		$(".budget_r").attr('disabled', 'disabled');
-		$(".uniform-choice span").addClass("disable_border")
-		$(".cprp_r").attr('disabled', 'disabled');
-	})
-	$("body").on("click", ".submit_budget", function(){
-		$("#budget_upload").show();
-		$(".budget").hide();
-		$(".complete_budget").show();
-		$(".cprp_r").attr('disabled', 'disabled');
-		$(".uniform-choice span").addClass("disable_border")
-		$(".budget_r").attr('disabled', 'disabled');
-	})
-
-	$("body").on("click", ".cprp_slip", function(){
-		$(".total_div").hide();
-	})
-	$("body").on("click", ".budget_slip", function(){
-		$(".total_div").hide();
-	})
-});
-</script> -->
 <body>
 
 	<!-- Main navbar -->
