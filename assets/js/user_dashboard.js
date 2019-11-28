8b8764f1a64f87c50968fe8354f4af2efeab1ae2
kavya <kavya.p@algonox.com>
2019-11-28 16:30:59 +0530

    replan css

--- assets/js/user_dashboard.js
+++ assets/js/user_dashboard.js
@@ -206,7 +206,7 @@ $(document).ready(function () {
                     ap += '<td  style="">'+v[i]['ClientName']+'</td>'
                     ap += '<td  style="">'+v[i]['BrandName']+'</td>'
                     // ap += '<td  style="text-align:center;">'+format_date(v[i]['StartDate'])+'</td>'
-                    ap += '<td> <button class="replanmodal" Campaignid="'+v[i]['CampaignId']+'" plainidattr="'+v[i]['PlanId']+'"  style="color:white!important;background-color: #f07144;;border: none;padding: 4PX;width: 68px;border-radius: 2px;">Re-Plan</button></td>';
+                    ap += '<td> <button class="replanmodal" Campaignid="'+v[i]['CampaignId']+'" plainidattr="'+v[i]['PlanId']+'"  style="color:white!important;background-color: #f07144;;border: none;padding: 4PX;width: 118px;border-radius: 2px;">Re-Plan</button></td>';
                     // ap += '<td  style="text-align:center;"><button class="replanmodal" campaign_id='+v[i]['CampaignId']+'  plainidattr="'+v[i]['PlanId']+'" style="background-color: #a5b1c2;color: #000;border: none;padding: 4PX;width: 68px;border-radius: 5px;">Re-Plan</button></td>'
                     ap += '<td  style="width:150px;">'+format_date(v[i]['EndDate'])+'</td>'
                     ap += '<td><div class="downloadbtn pointer" campId="'+v[i]['CampaignId']+'" plainidattr="'+v[i]['PlanId']+'" style=""><img src="assets/images/WhiteIcons/FilesDownload.png" style="width:27px;"></div></td>'
