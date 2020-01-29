46875d98c23dec277c37f9eaf90e827057360e7a
kavyachowdaryp <47849041+kavyachowdaryp@users.noreply.github.com>
2020-01-29 14:00:29 +0530

    url change

--- assets/js/common.js
+++ assets/js/common.js
@@ -62,20 +62,20 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
  function url() {
 
-    return 'http://192.168.1.33:6767/'
-        // return pythonurl
+    // return 'http://192.168.1.33:6767/'
+        return pythonurl
      // return 'https://cin-appsvplan-indtvauto-api-stg.azurewebsites.net/'
 
 }
 function url1() {
      // return 'http://192.168.0.164:6767/'
-    //  return pythonurl
-     return 'http://192.168.1.33:6767/'
+     return pythonurl
+     // return 'http://192.168.1.33:6767/'
 }
 function url2() {
      // return 'http://192.168.0.164:6767/'
-     return 'http://192.168.1.33:6767/'
-    //    return pythonurl
+     // return 'http://192.168.1.33:6767/'
+       return pythonurl
 }
 
 
